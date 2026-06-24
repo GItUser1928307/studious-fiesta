4750a80af36262741aa2a5d0b7f47603e8f879c9
omar772pro-afk <omar772.pro@gmail.com>
2026-06-25 00:37:19 +0300

    batch 32 for faster steps

--- local_ai/config.py
+++ local_ai/config.py
@@ -75,4 +75,4 @@ def auto_config():
 
 def auto_train_config(data_file="quick_train_data.txt", save_dir="quick_ckpt"):
     from system import get_avail_ram_gb
-    return TrainConfig(batch_size=64, max_steps=275, learning_rate=5e-4, log_interval=10, save_interval=50, data_file=data_file, save_dir=save_dir)
+    return TrainConfig(batch_size=32, max_steps=275, learning_rate=5e-4, log_interval=10, save_interval=50, data_file=data_file, save_dir=save_dir)
